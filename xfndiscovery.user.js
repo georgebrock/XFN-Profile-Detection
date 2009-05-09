e64201ffa671d01b0d1e06c19da83b7414856eb5
George Brocklehurst <george.brocklehurst@gmail.com>
2009-05-09 17:02:28 +0100

    Basic framework for a user script with jQuery.

---
+++ xfndiscovery.user.js
@@ -0,0 +1,8 @@
+// ==UserScript==
+// @name          XFN Profile Discovery
+// @namespace     http://georgebrock.com/
+// @description   Discover a user's oter profiles using the magic of XFN
+// @include       *
+// @require       http://code.jquery.com/jquery-latest.js
+// ==/UserScript==
+
